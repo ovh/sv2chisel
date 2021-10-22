2c3bc713c763077671357b522f88590e0fad4369
Jean Bruant <jean.bruant@ovhcloud.com>
2021-10-23 00:27:19 +0200

    Add HwEnum helpers with spec
    Signed-off-by: Jean Bruant <jean.bruant@ovhcloud.com>

---
+++ helpers/src/main/scala/HwEnum.scala
@@ -0,0 +1,41 @@
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Copyright 2020 The sv2chisel Authors. All rights reserved.
+
+package sv2chisel.helpers
+
+import chisel3._
+
+// require import scala.language.implicitConversions if not enabled in scalacOptions
+abstract class GenericHwEnum extends Enumeration {
+  def apply(): UInt = UInt(util.log2Up(this.values.size).W)
+  implicit def valueToUInt(x: Value): UInt = x.id.U
+}
+
+// Typical use-case example
+private object GenericHwEnumExample extends GenericHwEnum {
+  val stateA = Value
+  val stateB = Value
+  val stateC = Value
+}
+private object GenericHwEnumInlineExample extends GenericHwEnum {
+  val stateA, stateB, stateC = Value
+}
+
+
+// require import scala.language.implicitConversions if not enabled in scalacOptions
+abstract class CustomHwEnum extends Enumeration {
+  def getWidth: Int = this.values.map(_.getWidth).max
+  def apply(): UInt = UInt(this.getWidth.W)
+  
+  protected case class Val(data: Data) extends super.Val {}
+  implicit def valueToCustomVal(x: Value): Val = x.asInstanceOf[Val]
+  implicit def valueToUInt(x: Value): UInt = x.data.asUInt
+}
+
+private object CustomHwEnumExample extends CustomHwEnum {
+  val stateA = Val(0.U)
+  val stateB = Val(12.U)
+  val stateC = Val(5.U)
+}
+
---
+++ helpers/src/test/scala/HwEnumSpec.scala
@@ -0,0 +1,142 @@
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Copyright 2020 The sv2chisel Authors. All rights reserved.
+
+package sv2chiselHelpersTests
+
+import sv2chiselHelpersTests.utils._
+import sv2chisel.helpers._
+import sv2chisel.helpers.vecconvert._
+
+import chisel3._
+import chisel3.stage.ChiselStage
+
+import org.scalatest._
+import flatspec._
+
+class HwEnumSpec extends AnyFlatSpec with VerilogMatchers {
+  val setTestRunDir = Array("--target-dir", "test_run_dir")
+
+  "GenericHwEnum" should "just works" in {
+    
+    // warning: this type aliasing might hide bugs: confusing literal value (MyEnum.Value) with hardware type (UInt)
+    // type MyEnum = UInt // GenericHwEnum are simple UInt but provide syntax convenience
+    object MyEnum extends GenericHwEnum {
+      val stateA = Value
+      val stateB = Value
+      val stateC = Value
+    }
+    
+    class Example extends MultiIOModule {
+      val in       = IO(Input(Bool()))
+      val out      = IO(Output(MyEnum()))
+
+      val test = Wire(MyEnum())
+      when(in){
+        test := MyEnum.stateA
+      } .otherwise {
+        test := (MyEnum.maxId - 1).U // illustrate scala enumeration features  
+      }
+      
+      // typical reason why providing MyEnum aliasing is not a good idea
+      // def fun(en: MyEnum): MyEnum = en match {
+      //   case MyEnum.stateA => MyEnum.stateB // fruitless test : UInt against UInt literal => but compiler cannot warn
+      //   case MyEnum.stateB => MyEnum.stateA
+      //   case _ => println("always true"); en 
+      // }
+      // software: dealing with scala literal values
+      def funSw(en: MyEnum.Value): MyEnum.Value = en match {
+        case MyEnum.stateA => MyEnum.stateB // match scala literal values for equivalence
+        case MyEnum.stateB => MyEnum.stateA
+        case _ => en 
+      }
+      // hardware: dealing with signal and hardware literal
+      def funHw(en: UInt): UInt =  {
+        val res = Wire(en.cloneType)
+        when(en === MyEnum.stateA){ // implicit conversion to UInt => enabling to apply the equality in hardware
+          res := MyEnum.stateB // implicit conversion to UInt
+        } .elsewhen(en === MyEnum.stateB) {
+          res := MyEnum.stateA
+        } .otherwise {
+          res := en
+        }
+        res
+      }
+      
+      // when(fun(test) === fun(MyEnum.stateB)) { //quite risky for fun(test), basically doing nothing
+      // NB: using suggestName to get predictible name for intermediate verilog wires
+      when((funHw(test).suggestName("funHw") === funSw(MyEnum.stateB)).suggestName("bool")) {
+        out := test
+      } .otherwise {
+        out := 0.U
+      }
+    }
+    val verilog = (new ChiselStage()).emitVerilog(new Example(), setTestRunDir)
+    verilog should contains("wire [1:0] test = in ? 2'h0 : 2'h2;")
+    verilog should contains("wire  bool = funHw == 2'h0;")
+    verilog should contains("assign out = bool ? test : 2'h0;")
+
+  }
+  
+  "CustomHwEnum" should "just works" in {
+    
+    // warning: this type aliasing might hide bugs: confusing literal value (MyEnum.Value) with hardware type (UInt)
+    // type MyEnum = UInt // GenericHwEnum are simple UInt but provide syntax convenience
+    object MyEnum extends CustomHwEnum {
+      val stateA = Val(0.U)
+      val stateB = Val(12.U)
+      val stateC = Val(5.U)
+    }
+    
+    class Example extends MultiIOModule {
+      val in       = IO(Input(Bool()))
+      val out      = IO(Output(MyEnum()))
+
+      val test = Wire(MyEnum())
+      when(in){
+        test := MyEnum.stateA
+      } .otherwise {
+        test := MyEnum.stateB
+      }
+      
+      // typical reason why providing MyEnum aliasing is not a good idea
+      // def fun(en: MyEnum): MyEnum = en match {
+      //   case MyEnum.stateA => MyEnum.stateB // fruitless test : UInt against UInt literal => but compiler cannot warn
+      //   case MyEnum.stateB => MyEnum.stateA
+      //   case _ => println("always true"); en 
+      // }
+      // software: dealing with scala literal values
+      def funSw(en: MyEnum.Value): MyEnum.Value = en match {
+        case MyEnum.stateA => MyEnum.stateB // match scala literal values for equivalence
+        case MyEnum.stateB => MyEnum.stateA
+        case _ => en 
+      }
+      // hardware: dealing with signal and hardware literal
+      def funHw(en: UInt): UInt =  {
+        val res = Wire(en.cloneType)
+        when(en === MyEnum.stateA){ // implicit conversion to UInt => enabling to apply the equality in hardware
+          res := MyEnum.stateB // implicit conversion to UInt
+        } .elsewhen(en === MyEnum.stateB) {
+          res := MyEnum.stateA
+        } .otherwise {
+          res := en
+        }
+        res
+      }
+      
+      // when(fun(test) === fun(MyEnum.stateB)) { //quite risky for fun(test), basically doing nothing
+      // NB: using suggestName to get predictible name for intermediate verilog wires
+      when((funHw(test).suggestName("funHw") === funSw(MyEnum.stateA)).suggestName("bool")) {
+        out := test
+      } .otherwise {
+        out := 0.U
+      }
+    }
+    val verilog = (new ChiselStage()).emitVerilog(new Example(), setTestRunDir)
+    verilog should contains("wire [3:0] test = in ? 4'h0 : 4'hc;")
+    verilog should contains("wire  bool = funHw == 4'hc;")
+    verilog should contains("assign out = bool ? test : 4'h0;")
+
+  }
+  
+}
